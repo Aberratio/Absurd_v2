aead8cc30a906c0339415181ea4f29170c079edd
Louiks <kokot.jakub@wp.pl>
2020-08-04 15:24:28 +0200

    fixed a typing mistake

--- lang/lang_pl.php
+++ lang/lang_pl.php
@@ -67,7 +67,7 @@ class Infos
     public $send_comment = "Wyślij komentarz";
     public $explanation = "Wyjaśnienie";
         //Help Section
-    public $formating = "Formatowanie:";
+    public $formatting = "Formatowanie:";
     public $comment_help_text1 = "</br>Dwa znaki równości następujące po sobie odpowiadają eneterowi.";
     public $comment_help_text2 = "<b>Kto widzi Twóje komentarze?</b></br>Twój partner.</br>Twój trener (jeśli takiego na platformie posiadasz).</br>Administrator (ale ma za dużo roboty, żeby je czytać).</br>Pozostali użytkownicy nie mają dostępu do Twoich komentarzy!</br>";
     
